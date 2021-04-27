b1384c0abe975e39038f9683676f2778f96325f3
liyaSileshi <liyasileshi@gmail.com>
2021-04-27 12:52:06 -0700

    getting an error with json while extracting

--- extract_tweet.py
+++ extract_tweet.py
@@ -3,12 +3,10 @@ import os
 import json
 from dotenv import load_dotenv
 import pandas as pd
+pd.options.mode.chained_assignment = None  # default='warn'
 
 #load .env variables
 load_dotenv()
-
-# To set your enviornment variables in your terminal run the following line:
-# export 'BEARER_TOKEN'='<your_bearer_token>'
 
 def auth():
   """returns the BEARER TOKEN to access tweets based on id"""
@@ -27,46 +25,55 @@ def create_headers(bearer_token):
 
 def connect_to_endpoint(url, headers):
     response = requests.request("GET", url, headers=headers)
+    print(response.json())
     response_json_keys = response.json().keys()
+    # print(response_json_keys)
     # if status code is 200 or json returned an error
     if response.status_code != 200 or "errors" in response_json_keys:
       return None #if there url tweet id is invalid
     return response.json()
 
-def main():
-  bearer_token = auth()
-  url = create_url()
-  headers = create_headers(bearer_token)
-  json_response = connect_to_endpoint(url, headers)
-  print(json.dumps(json_response, indent=4, sort_keys=True))
-
 def nullify_tweet_col(df):
-    df['tweet'] = None
-    return df
+  """Adds a column named tweet will null values"""
+  df['tweet'] = None
+  return df
 
-def test_extract():
+def extract(df):
   bearer_token = auth()
-  df_test = pd.read_csv('./dataset/train.csv')
-  df = nullify_tweet_col(df_test)
-
+  df = nullify_tweet_col(df)
   #loop over df
-  for i in range(6):
+  for i in range(len(df)):
     ids = df['tweet_id'][i]
     url = create_url(ids)
-    
     headers = create_headers(bearer_token)
     json_response = connect_to_endpoint(url, headers)
-
     #if json response is not None
     if json_response is not None:
       #get the text
       text = json_response['data'][0]['text']
-      print(text)
       #append the text to the df 
       df['tweet'][i] = text
+  # print(df.head())
+  return df
 
-  print(df.head())
+def write_to_file(df, filename):
+  df.to_csv(f'./data_preprocess/{filename}')
+
+def main():
+   #get train data
+    # df_train = pd.read_csv('./dataset/train.csv')
+    # df = extract(df_train)
+    # filename = 'train.csv'
+    # write_to_file(df, filename)
+
+    #get test data
+    df_test = pd.read_csv('./dataset/test.csv')
+    df = extract(df_test)
+    filename = 'test.csv'
+    write_to_file(df, filename)
+
+    #get dev data
 
 if __name__ == "__main__":
-    # main()
-    test_extract()
+    main()
+    
